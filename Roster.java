7a529be677b9eaf567e53e722d05fa12c3439f62
Caleb <hamoncal25@7hills.org>
2024-01-16 12:49:45 -0500

    First Commit, starting to implement the people

---
+++ Roster.java
@@ -0,0 +1,20 @@
+public class Roster {
+    
+    private String emailIn;
+    private String nameIn;
+    private String firIn;
+    private String secIn;
+    private String thiIn;
+    private String fouIn;
+    private String fifIn;
+
+    public Roster(String emailG, String nameG, String firCh, String secCh, String thiCh, String fouCh, String fifCh){
+        emailIn = emailG;
+        nameIn = nameG;
+        firIn = firCh;
+        secIn = secCh;
+        thiIn = thiCh;
+        fouIn = fouCh;
+        fifIn = fifCh;
+    }
+}
