fec267aaa9f36d6d0741512d10ef960aa9194fc8
CalebHamon <hamoncal25@7hills.org>
2024-01-26 12:51:47 -0500

    creating a population in each class to ensure it does not go over

--- Sessions.java
+++ Sessions.java
@@ -2,11 +2,21 @@ public class Sessions {
     private String nameIn;
     private int popIn;
     private String alumIn;
+    private int peepIn;
 
     public Sessions(String nameG, int popG, String alumG){
         nameIn = nameG;
         popIn = popG;
         alumIn = alumG;
+        peepIn = 0;
+    }
+
+    public void addPeep(){
+        peepIn++;
+    }    
+
+    public int retPeep(){
+        return peepIn;
     }
 
     public String retName(){
